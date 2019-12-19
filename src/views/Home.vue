70a7d7d6e3ab937fe0341e471d0b8e18ee384299
dpr_fan <gbuecjd18@gmail.com>
2019-12-19 19:18:22 +0200

    initial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :items="books" :disableTo="2" @selected="selected"/>
   </div>
 </template>
 
@@ -13,6 +13,31 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      books: [
+        { id: 1, name: 'Harray potter 1', authorId: 1 },
+        { id: 2, name: 'Harray potter 2', authorId: 1 },
+        { id: 3, name: 'Harray potter 3', authorId: 1 },
+        { id: 4, name: 'Harray potter 4', authorId: 2 },
+        { id: 5, name: 'Harray potter 5', authorId: 2 },
+        { id: 6, name: 'Harray potter 6', authorId: 2 },
+        { id: 7, name: 'Harray potter 7', authorId: 2 },
+        { id: 8, name: 'Harray potter 8', authorId: 3 },
+        { id: 9, name: 'Harray potter 9', authorId: 3 },
+        { id: 10, name: 'Harray potter 10', authorId: 3 },
+        { id: 11, name: 'Harray potter 11', authorId: 1 },
+        { id: 12, name: 'Harray potter 12', authorId: 1 },
+        { id: 13, name: 'Harray potter 13', authorId: 1 },
+      ],
+      selectedBooks: []
+    }
+  },
+  methods: {
+    selected(selected) {
+      this.selectedBooks = selected
+    }
   }
 }
 </script>
